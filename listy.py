0786e998ed81f940bcf897b8a54b39fe9ce11f31
lopezgomezd8050 <112404788+lopezgomezd8050@users.noreply.github.com>
2022-11-16 09:48:12 -0600

    thingy thing tm

--- listy.py
+++ listy.py
@@ -1,1 +1,7 @@
+"""313"""
+robloxListy=[[['balls'], ['bbb', 'ccc']],[['realer', 'paramatery thingy lol xd', 'other param'], ['aaa']]]
+minecraftListy=[[['un'], ['ggg', 'fff']],[['realeeee', 'paramatery thingy lol xd'], ['eee']]]
+finalyListy=[['roblox',robloxListy],['minecraft',minecraftListy]]
+print(finalyListy)
 
+[['roblox', [[['balls'], ['bbb', 'ccc']], [['realer', 'paramatery thingy lol xd', 'other param'], ['aaa']]]], ['minecraft', [[['un'], ['ggg', 'fff']], [['realeeee', 'paramatery thingy lol xd'], ['eee']]]]]
